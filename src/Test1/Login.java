49b18fbcc5e472e12095be6e4facacee631862e4
umadevitenapalli <umadevitenapalli@gmail.com>
2025-01-06 17:29:42 +0530

    first commit

---
+++ src/Test1/Login.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class Login {
+	
+	//open chrome
+	WebDriver driver = new ChromeDriver();	
+			//open url
+	//click on login
+	//enter email
+	//click continue
+	//enter all details
+	//click submit
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module newwayfair {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+}
